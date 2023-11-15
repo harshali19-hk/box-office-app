f4eb906e9d7dee6e1f2cd4806c7e3ba57ca60019
harshali19-hk <kaleharshali1998@gmail.com>
2023-11-15 13:07:58 +0530

    used TVmaze API in Home.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
+
 
 
 const Home = () => {
+  
+ const [searchStr,setSearchStr] = useState("")
+
+ const handleInputChange = (eve)=>{
+  setSearchStr(eve.target.value)
+ }
+
+ const handleOnSearch = async (eve)=>{
+  eve.preventDefault()
+  const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+  const body = await response.json()
+   console.log(body)
+ }
+
   return (
     <div>
-        <h1>Home page</h1>
-        
+      <form onSubmit={handleOnSearch}>
+      <input type="text" value={searchStr} onChange={handleInputChange} /> 
+      <button type="submit">Search</button>
+      </form>
     </div>
   )
 }
