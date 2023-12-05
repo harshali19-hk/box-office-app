4ef6084e99017b5713e4177dac059b45d4216b53
harshali19-hk <kaleharshali1998@gmail.com>
2023-12-05 09:42:19 +0530

    added fade-in animation to flex grid. addedscale animation to starbtn.

--- src/components/shows/ShowsCard.jsx
+++ src/components/shows/ShowsCard.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../Commom/SearchCard';
 import { StarIcon } from '../Commom/StartIcon';
+import { useRef } from 'react';
 
 const ShowsCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
@@ -10,6 +11,23 @@ const ShowsCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
         .slice(0, 10)
         .join(' ')
         .replace(/<.+?>/g, ' ') ;
+
+  const starBtnRef = useRef()
+
+  const handleStarMeClick = ()=>{
+     onStarMeClick(id)
+
+     const starBtnElement = starBtnRef.current
+     if(!starBtnElement) return;
+
+     if(isStarred){
+      starBtnElement.classList.remove('animate')
+     }
+     else{
+      starBtnElement.classList.add('animate')
+     }
+  }
+
   return (
     <SearchCard>
       <SearchImgWrapper>
@@ -21,7 +39,11 @@ const ShowsCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read More
         </a>
-        <StarBtn onClick={() => onStarMeClick(id)}>
+        <StarBtn 
+        ref={starBtnRef}
+        type='button'
+        onClick={handleStarMeClick} 
+        >
           <StarIcon active={isStarred} />
           {/* {isStarred ? 'Unstar Me' : 'Star Me'} */}
         </StarBtn>
@@ -46,7 +68,7 @@ const ActionSection = styled.div`
     }
   }
 `;
-
+ 
 const StarBtn = styled.button`
   outline: none;
   border: 1px solid #8e8e8e;
@@ -58,5 +80,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
